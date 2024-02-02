cf4bc7835c520e5fcab8029db9ab48637e381b49
Arjit Gupta <devgupta0304@gmail.com>
2024-02-02 11:00:49 +0530

    Udating app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import faiss
+import pickle
 
 #Sidebar contents
 with st.sidebar:
@@ -13,3 +18,40 @@ with st.sidebar:
     ''')
     add_vertical_space (5)
     st.write('(https://youtube.com/@engineerprompt)')
+
+
+def main():
+    st.write('Hello')
+
+    pdf_file = st.file_uploader("Upload your file", type='pdf')
+    
+    
+    if pdf_file is not None:
+        st.write(pdf_file.name)
+        pdf_reader = PdfReader(pdf_file)
+
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        
+        text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+        )
+
+        chunks = text_splitter.split_text(text=text)
+
+
+        embeddings_obj = OpenAIEmbeddings()
+
+        vector_store = faiss.from_texts(chunks, embeddings_obj)
+
+        store_name = pdf_file.name[:-4]
+        with open (f"{store_name}.pkl", "wb") as file:
+            pickle.dump(vector_store, f)
+        # st.write(chunks)
+
+
+if __name__ == '__main__':
+    main()
